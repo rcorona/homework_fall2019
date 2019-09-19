2dd4bb63ffd5f5f9d88fc53b531b2df04d1ded47
Rodolfo Corona <rcorona@berkeley.edu>
2019-09-19 23:07:07 +0000

    progress

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -108,14 +108,49 @@ class MLPPolicy(BasePolicy):
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs):
 
-        # TODO: GETTHIS from HW1
+        if len(obs.shape)>1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        feed_dict = {self.observations_pl: observation}
+        
+        ac = self.sess.run(self.sample_ac, feed_dict=feed_dict)
+        
+        return ac
 
 #####################################################
 #####################################################
 
-# class MLPPolicySL(MLPPolicy):
+class MLPPolicySL(MLPPolicy):
 
-    # TODO: GETTHIS from HW1 (or comment it out, since you don't need it for this homework)
+    """
+        This class is a special case of MLPPolicy,
+        which is trained using supervised learning.
+        The relevant functions to define are included below.
+    """
+
+    def define_placeholders(self):
+        # placeholder for observations
+        self.observations_pl = tf.placeholder(shape=[None, self.ob_dim], name="ob", dtype=tf.float32)
+
+        # placeholder for actions
+        self.actions_pl = tf.placeholder(shape=[None, self.ac_dim], name="ac", dtype=tf.float32)
+
+        if self.training:
+            self.acs_labels_na = tf.placeholder(shape=[None, self.ac_dim], name="labels", dtype=tf.float32)
+
+    def define_train_op(self):
+        true_actions = self.acs_labels_na
+        predicted_actions = self.sample_ac
+
+        self.loss = tf.losses.mean_squared_error(true_actions, predicted_actions)
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+
+    def update(self, observations, actions):
+        assert(self.training, 'Policy must be created with training=True in order to perform training updates...')
+        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={self.observations_pl: observations, self.acs_labels_na: actions})
+        return loss
 
 #####################################################
 #####################################################
@@ -157,20 +192,29 @@ class MLPPolicyPG(MLPPolicy):
             # to get [Q_t - b_t]
         # HINT4: don't forget that we need to MINIMIZE this self.loss
             # but the equation above is something that should be maximized
-        self.loss = tf.reduce_sum(TODO)
+        
+        # Take negative log likelihood weighted by advantage. 
+        # TODO is this advantage actually computed already or do we compute it ourselves?? 
+        self.loss = - self.logprob_n * self.adv_n
+        print(self.loss.shape)
+        exit()
+        self.loss = tf.reduce_sum(self.loss) # TODO get shape right. 
 
         # TODO: define what exactly the optimizer should minimize when updating the policy
-        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(TODO)
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
         if self.nn_baseline:
             # TODO: define the loss that should be optimized for training the baseline
             # HINT1: use tf.losses.mean_squared_error, similar to SL loss from hw1
             # HINT2: we want predictions (self.baseline_prediction) to be as close as possible to the labels (self.targets_n)
                 # see 'update' function below if you don't understand what's inside self.targets_n
-            self.baseline_loss = TODO
+            print(self.targets_n.shape)
+            print(self.baseline_prediction.shape)
+            exit() # TODO Make sure shape is correct. 
+            self.baseline_loss = tf.losses.mean_squared_error(self.targets_n, self.baseline_prediction)
 
             # TODO: define what exactly the optimizer should minimize when updating the baseline
-            self.baseline_update_op = tf.train.AdamOptimizer(self.learning_rate).minimize(TODO)
+            self.baseline_update_op = tf.train.AdamOptimizer(self.learning_rate).minimize(baseline_loss)
 
     #########################
 
@@ -180,7 +224,15 @@ class MLPPolicyPG(MLPPolicy):
         # HINT1: query it with observation(s) to get the baseline value(s)
         # HINT2: see build_baseline_forward_pass (above) to see the tensor that we're interested in
         # HINT3: this will be very similar to how you implemented get_action (above)
-        return TODO
+        if len(obs.shape)>1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        feed_dict = {self.observations_pl: observation}
+        baseline_pred = sess.run(self.baseline_prediction, feed_dict=feed_dict)
+
+        return baseline_pred
 
     def update(self, observations, acs_na, adv_n=None, acs_labels_na=None, qvals=None):
         assert(self.training, 'Policy must be created with training=True in order to perform training updates...')
@@ -191,7 +243,8 @@ class MLPPolicyPG(MLPPolicy):
             targets_n = (qvals - np.mean(qvals))/(np.std(qvals)+1e-8)
             # TODO: update the nn baseline with the targets_n
             # HINT1: run an op that you built in define_train_op
-            TODO
+            _, baseline_loss = self.sess.run([self.baseline_update_op, self.baseline_loss], feed_dict={self.observations_pl: observations})
+
         return loss
 
 #####################################################
